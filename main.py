bd310c31c9e7c77805dcbf11195a4f2cfcef57c4
Jean Diniz <jean.diniz@youxgroup.com.br>
2023-12-10 11:17:30 -0300

    Finished DFA, DPDA, NFA

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import uvicorn
 from automata.fa.dfa import DFA
-from automata.pda.dpda import DPDA
 from automata.fa.nfa import NFA
-from typing import Union
-
+from automata.pda.dpda import DPDA
 from fastapi import FastAPI, Request, HTTPException
 
 app = FastAPI()
@@ -17,29 +15,7 @@ def read_root():
 @app.post("/dfa")
 async def automata(request: Request):
     info = await request.json()
-    states = set(info.get("states", []))
-    if len(states) == 0:
-        raise HTTPException(status_code=400, detail="Estado não pode ser vazio")
-
-    input_symbols = set(info.get("input_symbols", []))
-    if len(input_symbols) == 0:
-        raise HTTPException(status_code=400, detail="Simbolos não pode ser vazio")
-
-    transitions = dict(info.get("transitions", {}))
-    if len(transitions) == 0:
-        raise HTTPException(status_code=400, detail="Transições não pode ser vazio")
-
-    initial_state = info.get("initial_state", "")
-    if initial_state == "":
-        raise HTTPException(status_code=400, detail="Estado inicial não pode ser vazio")
-
-    final_states = set(info.get("final_states", ""))
-    if len(final_states) == 0:
-        raise HTTPException(status_code=400, detail="Estado final não pode ser vazio")
-
-    input_w = info.get("input_w", "")
-    if input_w == "":
-        raise HTTPException(status_code=400, detail="Input não pode ser vazio")
+    final_states, initial_state, input_symbols, input_w, states, transitions = getJsonData(info)
 
     dfa = DFA(
         states=states,
@@ -58,37 +34,15 @@ async def automata(request: Request):
 @app.post("/dpda")
 async def automata(request: Request):
     info = await request.json()
-    states = set(info.get("states", []))
-    if len(states) == 0:
-        raise HTTPException(status_code=400, detail="Estado não pode ser vazio")
-
-    input_symbols = set(info.get("input_symbols", []))
-    if len(input_symbols) == 0:
-        raise HTTPException(status_code=400, detail="Simbolos não pode ser vazio")
-
-    stack_symbols = set(info.get("stack_symbols", []))
-    if len(stack_symbols) == 0:
-        raise HTTPException(status_code=400, detail="Stack simbolos não pode ser vazio")
-
-    transitions = dict(info.get("transitions", {}))
-    if len(transitions) == 0:
-        raise HTTPException(status_code=400, detail="Transições não pode ser vazio")
-
-    initial_state = info.get("initial_state", "")
-    if initial_state == "":
-        raise HTTPException(status_code=400, detail="Estado inicial não pode ser vazio")
+    final_states, initial_state, input_symbols, input_w, states, transitions = getJsonData(info)
 
     initial_stack_symbol = info.get("initial_stack_symbol", "")
     if initial_stack_symbol == "":
         raise HTTPException(status_code=400, detail="Estado inicial da stack não pode ser vazio")
 
-    final_states = set(info.get("final_states", ""))
-    if len(final_states) == 0:
-        raise HTTPException(status_code=400, detail="Estados finais não pode ser vazio")
-
-    input_w = info.get("input_w", "")
-    if input_w == "":
-        raise HTTPException(status_code=400, detail="Input não pode ser vazio")
+    stack_symbols = set(info.get("stack_symbols", []))
+    if len(stack_symbols) == 0:
+        raise HTTPException(status_code=400, detail="Stack simbolos não pode ser vazio")
 
     dpda = DPDA(
         states=states,
@@ -110,29 +64,7 @@ async def automata(request: Request):
 @app.post("/nfa")
 async def automata(request: Request):
     info = await request.json()
-    states = set(info.get("states", []))
-    if len(states) == 0:
-        raise HTTPException(status_code=400, detail="Estado não pode ser vazio")
-
-    input_symbols = set(info.get("input_symbols", []))
-    if len(input_symbols) == 0:
-        raise HTTPException(status_code=400, detail="Simbolos não pode ser vazio")
-
-    transitions = dict(info.get("transitions", {}))
-    if len(transitions) == 0:
-        raise HTTPException(status_code=400, detail="Transições não pode ser vazio")
-
-    initial_state = info.get("initial_state", "")
-    if initial_state == "":
-        raise HTTPException(status_code=400, detail="Estado inicial não pode ser vazio")
-
-    final_states = set(info.get("final_states", ""))
-    if len(final_states) == 0:
-        raise HTTPException(status_code=400, detail="Estado final não pode ser vazio")
-
-    input_w = info.get("input_w", "")
-    if input_w == "":
-        raise HTTPException(status_code=400, detail="Input não pode ser vazio")
+    final_states, initial_state, input_symbols, input_w, states, transitions = getJsonData(info)
 
     nfa = NFA(
         states=states,
@@ -142,10 +74,32 @@ async def automata(request: Request):
         final_states=final_states
     )
 
-    if nfa.read_input(input_w):
+    if nfa.accepts_input(input_w):
         return {"message": "Aceita"}
     else:
         return {"message": "Rejeita"}
+
+
+def getJsonData(json):
+    states = set(json.get("states", []))
+    if len(states) == 0:
+        raise HTTPException(status_code=400, detail="Estado não pode ser vazio")
+    input_symbols = set(json.get("input_symbols", []))
+    if len(input_symbols) == 0:
+        raise HTTPException(status_code=400, detail="Simbolos não pode ser vazio")
+    transitions = dict(json.get("transitions", {}))
+    if len(transitions) == 0:
+        raise HTTPException(status_code=400, detail="Transições não pode ser vazio")
+    initial_state = json.get("initial_state", "")
+    if initial_state == "":
+        raise HTTPException(status_code=400, detail="Estado inicial não pode ser vazio")
+    final_states = set(json.get("final_states", ""))
+    if len(final_states) == 0:
+        raise HTTPException(status_code=400, detail="Estados finais não pode ser vazio")
+    input_w = json.get("input_w", "")
+    if input_w == "":
+        raise HTTPException(status_code=400, detail="Input não pode ser vazio")
+    return final_states, initial_state, input_symbols, input_w, states, transitions
 
 
 if __name__ == "__main__":
